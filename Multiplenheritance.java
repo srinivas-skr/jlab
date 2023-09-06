6fa30b5ebf541fcabe33553980da905ffc17f79a
srinivas-skr <124112058+srinivas-skr@users.noreply.github.com>
2023-09-06 21:14:51 +0530

    Update Multiplenheritance.java

--- Multiplenheritance.java
+++ Multiplenheritance.java
@@ -1,30 +1,41 @@
-package lab;
-import java.util.Scanner;
+package srini;
 
+import java.util.*;
+    // First interface representing a shape
+    interface Shape {
+        double Area();
+    }
 
-interface vehicleone{
-int speed=90;
-public void distance();
-}
+    // Second interface representing a color
+    interface Color {
+        String Color();
+    }
 
-interface vehicletwo{
-int distance=100;
-public void speed();
-}
-class Vehicle implements vehicleone,vehicletwo{
-public void distance(){
-int distance=speed*100;
-System.out.println("distance travelled is "+distance);
-}
-public void speed(){
-int speed=distance/100;
-}
-}
-class MultipleInheritanceUsingInterface{
-public static void main(String args[]){
-System.out.println("Vehicle");
-Vehicle obj = new Vehicle();
-obj.distance();
-obj.speed();
-}
-}
+    // Concrete class Circle implementing Shape and Color interfaces
+    class Circle implements Shape, Color {
+        private double radius;
+        private String color;
+
+        public Circle(double radius, String color) {
+            this.radius = radius;
+            this.color = color;
+        }
+
+        @Override
+        public double Area() {
+            return Math.PI * radius * radius;
+        }
+
+        @Override
+        public String Color() {
+            return color;
+        }
+    }
+
+    public class multi {
+        public static void main(String[] args) {
+            Circle ob = new Circle(5.0, "Red");
+            System.out.println("Circle Area: " + ob.Area());
+            System.out.println("Circle Color: " + ob.Color());
+        }
+    }
